07dfe440c21a1bda4e25928a15c8431de4ab8c46
Akhileshbhagat1 <akhileshkrbhagat666@gmail.com>
2020-02-03 13:51:02 +0530

    first commit

---
+++ jobs/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# Create your models here.
+class Job(models.Model):
+    image = models.ImageField(upload_to='images/')  #here image model and source to save value
+    summary = models.CharField(max_length=100)
+ 
